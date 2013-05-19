32ca2d7b3d0805171e1c2d9a04bc241eca9866ff
Brian  Hall <hall.briandavis@gmail.com>
2013-05-19 17:33:16 +0800

    with CSS

--- DynamicQuizJS.js
+++ DynamicQuizJS.js
@@ -1,5 +1,16 @@
+/* To add
+-If blank, shake screen and prompt answer
+-— Add client-side data validation: make sure the user answers each question before proceeding to the next question.
+— Add a “Back” button to allow the user to go back and change her answer. The user can go back up to the first question. For the questions that the user has answered already, be sure to show the radio button selected, so that the user is not forced to answer the questions again, which she has completed.
+— Use jQuery to add animation (fade out the current question and fade in the next question).
+— Test the quiz on IE 8 and 9, and fix any bugs. This will give you a good workout
+— Store the quiz questions in an external JSON file.
+— Add user authentication: allow users log in, and save their login credentials to local storage (HTML5 browser storage).
+— Use cookies to remember the user, and show a “Welcome, First Name” message when the user returns to the quiz.
+*/
+
 window.onload = function() {
-window.alert("Hello");
+
 var allQuestions = [{
     question: "What is 4x6?",
     choices: [46, 15, 25, 24],
